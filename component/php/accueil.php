7f7156dbd3e17998adc3704e95bd1f6ad2e60256
pharell <86664849+Pharell77coder@users.noreply.github.com>
2024-02-19 12:03:37 +0100

    Add files via upload

--- component/php/accueil.php
+++ component/php/accueil.php
@@ -1,18 +1,14 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <meta name="author" content="Pharell">
-    <link rel="stylesheet" href="../css/main.css">
+    <?php include "include/head.php"; ?>
 </head>
 <body>
     <?php
 
-    include "header.php";
-    include "nav.php";
-    include "footer.php";
+    include "include/header.php";
+    include "include/nav.php";
+    include "include/footer.php";
 
     $dbFile = '../db/data.db';
 
